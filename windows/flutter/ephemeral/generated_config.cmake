710566a1f12640626903ba237dfc691e83462e98
mogumogu360 <boseashmita02@gmail.com>
2024-12-28 09:56:37 +0530

    Initial commit

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,23 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\videoSDK assignment\\videosdk-rtc-flutter-sdk-example" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+14" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 14 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\flutter"
+  "PROJECT_DIR=C:\\videoSDK assignment\\videosdk-rtc-flutter-sdk-example"
+  "FLUTTER_ROOT=C:\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\videoSDK assignment\\videosdk-rtc-flutter-sdk-example\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\videoSDK assignment\\videosdk-rtc-flutter-sdk-example"
+  "FLUTTER_TARGET=C:\\videoSDK assignment\\videosdk-rtc-flutter-sdk-example\\lib\\main.dart"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\videoSDK assignment\\videosdk-rtc-flutter-sdk-example\\.dart_tool\\package_config.json"
+)
